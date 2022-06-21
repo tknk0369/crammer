906d76ae09247f93350fe1248865d31fcd5a39b4
tknk0369 <tknk03690369@gmail.com>
2022-06-21 22:10:41 +0900

    first commit

---
+++ app/src/main/java/io/github/tknk0369/crammer/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package io.github.tknk0369.crammer.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
